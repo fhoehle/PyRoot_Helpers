276de7e1d957c87d62fab671c9e3420ebc2f4229
HOEHLE Felix <hoehle@physik.rwth-aachen.de>
2013-05-22 23:42:04 +0200

    tidy up MyHistFunctions, no need of set maximum and added awsome functions for statbox and canvas sorting

--- PyRoot_Functions/PadFunctions_cfi.py
+++ PyRoot_Functions/PadFunctions_cfi.py
@@ -11,8 +11,7 @@ def adaptMaxMinOfPad(pad = ROOT.gPad,fac = 0.15):
       max = obj.GetMaximum() if obj.GetMaximum() > max else max
       min = obj.GetMinimum() if obj.GetMinimum() < max else min
   if first != None:
-    first.SetMaximum((1+fac)*max)
-    first.SetMinimum((1-fac)*min)
+    first.GetYaxis().SetRangeUser((1-fac)*min,(1+fac)*max)
   pad.Update()
 def getHistsOfPad(pad = ROOT.gPad, type = ROOT.TH1F):
   objs = pad.GetListOfPrimitives()
@@ -21,12 +20,43 @@ def getHistsOfPad(pad = ROOT.gPad, type = ROOT.TH1F):
     if isinstance(obj,type):
       hists.append(obj)
   return hists
+######################################################
 import StatBoxFunctions_cfi
 def getStatboxsOfPad(pad = ROOT.gPad):
   objs = pad.GetListOfPrimitives()
   first = None; 
   statboxs = []
   for obj in objs:
-    if isinstance(obj,ROOT.TH1F):
+    if isinstance(obj,ROOT.TH1):
       statboxs.append(StatBoxFunctions_cfi.GetStatBox(obj))
-  return statboxs   
+  return statboxs  
+####################
+def statboxesRightSide(pad=ROOT.gPad):
+   statboxs = getStatboxsOfPad(pad)
+   tmpbox = statboxs[0]
+   oldPadWidth = pad.GetWw()
+   padHeight = pad.GetWh()
+   statboxWidth = (tmpbox.GetX2NDC()-tmpbox.GetX1NDC())*oldPadWidth
+   statboxHeight = (tmpbox.GetY2NDC()-tmpbox.GetY1NDC())*padHeight
+   rightMarginWidth = pad.GetRightMargin()*oldPadWidth
+   leftMarginWidth = pad.GetLeftMargin()*oldPadWidth
+   histWidth = (1-pad.GetRightMargin()-pad.GetLeftMargin())*oldPadWidth
+   noStatboxsPerColumn = int((padHeight*(1-pad.GetTopMargin() - pad.GetBottomMargin()))/statboxHeight)
+   columnsStatBoxes = int(len(statboxs)/noStatboxsPerColumn +1)
+   #print "leftMarginWidth ",leftMarginWidth," statboxHeight ",statboxHeight," statboxWidth ",statboxWidth
+   #print "leftMarginWidth ",leftMarginWidth," histWidth ",histWidth,"  Ww ",pad.GetWw()
+   pad.SetWindowSize(int(leftMarginWidth+histWidth+statboxWidth*columnsStatBoxes+rightMarginWidth),padHeight)
+   newPadWidth = pad.GetWw()
+   pad.SetRightMargin((rightMarginWidth+statboxWidth*columnsStatBoxes)/newPadWidth)
+   pad.SetLeftMargin(leftMarginWidth/newPadWidth)
+   if columnsStatBoxes > 1 :
+     print "too many histograms "
+   for i,statbox in enumerate(statboxs):
+#     print "setting statbox ",statbox.GetName()," X1NDC ",statbox.GetX1NDC()," to ",leftMarginWidth+histWidth
+#     print "setting statbox ",statbox.GetName()," X2NDC ",statbox.GetX2NDC()," to ",leftMarginWidth+histWidth+statboxWidth
+     statbox.SetX1NDC((leftMarginWidth+histWidth+int(i/noStatboxsPerColumn)*statboxWidth)/newPadWidth);statbox.SetX2NDC((leftMarginWidth+histWidth+(int(i/noStatboxsPerColumn)+1)*statboxWidth)/newPadWidth);
+     statbox.SetY1NDC((padHeight-pad.GetTopMargin()*padHeight-(i+1-noStatboxsPerColumn*int(i/noStatboxsPerColumn))*statboxHeight)/padHeight);statbox.SetY2NDC((padHeight-pad.GetTopMargin()*padHeight-(i-noStatboxsPerColumn*int(i/noStatboxsPerColumn))*statboxHeight)/padHeight);
+#     print "setting statbox ",statbox.GetName()," Y1NDC ",statbox.GetY1NDC()," to ",pad.GetWh()-(i+1)*statboxHeight
+   pad.Update()
+   pad.Modified()
+   pad.Update()
